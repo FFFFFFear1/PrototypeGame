1a92668ffcdb0556a64dc8a97c040c31b00ab619
Kostya Ozherelev <korlite719@gmail.com>
2021-10-11 20:55:14 +0300

    First commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smooth = 0.125f;
+
+    private void LateUpdate()
+    {
+        var newPosition = target.position + offset;
+        var smoothedPosition = Vector3.Lerp(transform.position, newPosition, smooth);
+
+        transform.position = new Vector3(0, smoothedPosition.y, smoothedPosition.z);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+
+    private float speedModifier = 0.01f;
+
+    private Vector3 startPositionMouse;
+    private Vector3 startPositionPlayer;
+
+    private Animator playerAnim;
+
+    private void Start()
+    {
+        playerAnim = GetComponent<Animator>();
+
+        startPositionPlayer = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPositionMouse = Input.mousePosition;
+            playerAnim.SetBool("isRun", true);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            var directionX = startPositionMouse.x - Input.mousePosition.x;
+            var x = directionX * speedModifier;
+
+            transform.position = new Vector3(
+                startPositionPlayer.x + x,
+                transform.position.y,
+                transform.position.z + (speed * -1) * Time.deltaTime); ;
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            startPositionPlayer = transform.position;
+            playerAnim.SetBool("isRun", false);
+        }
+    }
+}
