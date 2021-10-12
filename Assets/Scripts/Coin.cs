da416210e117b0c4d41bc2bbabbdb933fec46df2
FFFFFFear1 <sasha1936@list.ru>
2021-10-12 15:57:06 +0300

    Added coins + score

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private GameObject coinParticle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            UIController.instance.CoinsCount++;
+            Instantiate(coinParticle, transform.position, transform.rotation);
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI coinsText;
+
+    private int coinsCount;
+
+    public Action OnCoinsChanged;
+
+    public static UIController instance;
+
+    void Start()
+    {
+        instance = this;
+        OnCoinsChanged += UpdateCoinsView;
+    }
+
+    private void UpdateCoinsView()
+    {
+        coinsText.text = $"Coins: {coinsCount}";
+    }
+
+    public int CoinsCount
+    {
+        get { return coinsCount; }
+        set 
+        { 
+            coinsCount += value;
+            OnCoinsChanged();
+        }
+    }
+}
